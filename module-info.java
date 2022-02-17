5bdda6e8e1e4b55ad777837cab8a31c31a9ed444
Anastasiya Pupkevich <nasta978@mail.ru>
2022-02-17 21:38:01 +0300

    added String module-info.java

--- module-info.java
+++ module-info.java
@@ -1,2 +1,17 @@
-module Proect1Git {
+
+public class ProjectGit {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=5;
+		b=3;
+		int c=a+b;
+		String v = "g"+"i"+"t";
+		System.out.println ("v = " +v);
+		System.out.println ("c = " +c);
+		int z = c-a+b*2;
+		System.out.println ("z = " +z);
+
+	}
+
 }
