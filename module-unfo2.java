9593a94d955ed966a5d7438b711f1298ad9f71a5
Anastasiya Pupkevich <nasta978@mail.ru>
2022-02-17 21:17:37 +0300

    added 2 module-info.java

---
+++ module-unfo2.java
@@ -0,0 +1,15 @@
+
+public class ProjectGit {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=5;
+		b=3;
+		int c=a+b;
+		System.out.println ("c = " +c);
+		int z = c-a+b*2;
+		System.out.println ("z = " +z);
+
+	}
+
+}
